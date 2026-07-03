4a81f24a3f3666a4ee02d527fafcdc58d934ceed
Ilkka Lehtoranta <ilkka.lehtoranta@valmet.com>
2026-07-03 19:39:10 +0300

    Expanded static code analyzers

--- HotPathAttributes.cs
+++ HotPathAttributes.cs
@@ -2,9 +2,14 @@ using System;
 
 namespace CopperScreen;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Constructor)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Property)]
 internal sealed class HotPathAttribute : Attribute
 {
+    /// <summary>
+    /// Maximum number of branch points (if, switch cases, ternary, null coalescing, etc.) allowed in the method body.
+    /// Zero means no limit (default). The analyzer reports CMPERF006 if exceeded.
+    /// </summary>
+    public int MaxBranches { get; set; }
 }
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Constructor)]
