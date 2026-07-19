1666a031ec343af1043c3b453c07decf4de64fac
Ilkka Lehtoranta <ilkka.lehtoranta@valmet.com>
2026-07-20 02:26:03 +0300

    Simplified register model

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System.Runtime;
+using System.Runtime.Loader;
 using System.Diagnostics;
 using Avalonia;
+using CopperPad;
 
 namespace CopperScreen;
 
@@ -13,6 +15,7 @@ internal static class Program
 	{
 		StartupArgs = args ?? Array.Empty<string>();
 		CopperScreenCrashLog.Install(StartupArgs);
+		InstallCopperPadAssemblyResolver();
 		try
 		{
 			Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
@@ -29,6 +32,17 @@ internal static class Program
 		{
 			CopperScreenCrashLog.Shutdown();
 		}
+	}
+
+	private static void InstallCopperPadAssemblyResolver()
+	{
+		// The current CopperPad.HidSharp preview requests CopperPad 2.0.0.0,
+		// while its companion package ships the same API as assembly version 1.0.0.0.
+		var copperPadAssembly = typeof(CopperControllerHost).Assembly;
+		AssemblyLoadContext.Default.Resolving += (_, requestedAssembly) =>
+			string.Equals(requestedAssembly.Name, copperPadAssembly.GetName().Name, StringComparison.Ordinal)
+				? copperPadAssembly
+				: null;
 	}
 
 	private static AppBuilder BuildAvaloniaApp()
